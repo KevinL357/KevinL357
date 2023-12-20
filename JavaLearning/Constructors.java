bb1278ef9c293c3b381f3dd74687fe162466ab38
Kevin Li <118126174+KevinL357@users.noreply.github.com>
2023-12-20 18:23:34 -0500

    new updates
    more user friendly ziyao inputs

--- JavaLearning/Constructors.java
+++ JavaLearning/Constructors.java
@@ -1,5 +1,18 @@
+class Masheen {
+    public String naem;
+
+    public Masheen(String naem) {
+        System.out.print("Konstructer runing! " + naem);
+    }
+}
+
+
+
 public class Constructors {
     public static void main(String[] args) {
-        
+        Masheen masheen = new Masheen("herro");
+
+        System.out.println(masheen.naem);
+
     }
 }
---
+++ SideProjects/ZiyaosProclamation.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class ZiyaosProclamation {
+
+    String[] ziyaosMessage = new String[] {"goof", "yourself", "right", "now!"};
+
+    int attempts = 0;
+    int correct = 0;
+
+    int goal;
+
+    String guess;
+    int guessNumner = 0;
+
+    Scanner gamble;
+    
+    public ZiyaosProclamation(int min, int max) {
+
+        gamble = new Scanner(System.in);
+
+        System.out.println("Enter a numner to start your game!");
+
+        while (correct < 4 && attempts <= 20)
+        {
+            goal = (int) (min + Math.random() * (max - min));
+            guess = gamble.next();
+
+            try {
+                guessNumner = Integer.parseInt(guess);
+            } catch (Exception e) {}
+
+            if (guessNumner == goal) {
+                System.out.println(ziyaosMessage[correct]);
+                correct++;
+            } else System.out.println("gys");
+            
+            System.out.println();
+            attempts++;
+
+        }
+
+        if (correct == 4)
+            System.out.println("congratulations. You took " + attempts + " attempts.");
+        else
+            System.out.println("Ziyao's message will not be decoded!");
+    }
+}
